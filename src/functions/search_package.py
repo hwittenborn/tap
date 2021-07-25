1446c7fb71d09a3c62e5653f5238fdc6388b3a5e
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-07-25 02:51:06 -0500

    Moved arround arangement of code in search_package.py to look neater [CI SKIP]

--- src/functions/search_package.py
+++ src/functions/search_package.py
@@ -25,19 +25,29 @@ def search_package(mpr_url, packages, application_name, application_version):
 
 	while number < mpr_rpc_json_data["resultcount"]:
 
-		print(f"{colors.apt_green}{mpr_rpc_json_data['results'][number]['Name']}{colors.white}/{mpr_rpc_json_data['results'][number]['Version']}")
-		print(f"  Description: {mpr_rpc_json_data['results'][number]['Description']}")
-		print(f"  Maintainer: {mpr_rpc_json_data['results'][number]['Maintainer']}")
-		print(f"  Votes: {str(mpr_rpc_json_data['results'][number]['NumVotes'])}")
+		# Get JSON data
+		package_name = mpr_rpc_json_data['results'][number]['Name']
+		package_version = mpr_rpc_json_data['results'][number]['Version']
+		package_description = mpr_rpc_json_data['results'][number]['Description']
+		package_maintainer = mpr_rpc_json_data['results'][number]['Maintainer']
+		package_votes = mpr_rpc_json_data['results'][number]['NumVotes']
 
 		outofdate = mpr_rpc_json_data['results'][number]['OutOfDate']
 		if outofdate == None:
-			print("  Out of Date: N/A")
+			package_outofdate = "N/A"
 		else:
-			print(f"  Out of Date: {datetime.datetime.fromtimestamp(outofdate).strftime('%Y-%m-%d')}")
+			package_outofdate = datetime.datetime.fromtimestamp(outofdate).strftime('%Y-%m-%d')
 
-		lastmodified = mpr_rpc_json_data['results'][number]['LastModified']
-		print(f"  Last Modified: {datetime.datetime.fromtimestamp(lastmodified).strftime('%Y-%m-%d')}")
+		package_lastmodified_json = mpr_rpc_json_data['results'][number]['LastModified']
+		package_lastmodified = datetime.datetime.fromtimestamp(package_lastmodified_json).strftime('%Y-%m-%d')
+
+		# Print generated text
+		print(f"{colors.apt_green}{package_name}{colors.white}/{package_version}")
+		print(f"  Description: {package_description}")
+		print(f"  Maintainer: {package_maintainer}")
+		print(f"  Votes: {package_votes}")
+		print(f"  Out of Date: {package_outofdate}")
+		print(f"  Last Modified: {package_lastmodified}")
 
 		if (number + 1) < mpr_rpc_json_data["resultcount"]:
 			print()
