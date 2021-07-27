9f94712c08c6e3a026bf29499c724b86f5480ad5
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-07-27 01:18:29 -0500

    Added description and maintainer to 'list-packages' output

--- src/functions/list_packages.py
+++ src/functions/list_packages.py
@@ -4,26 +4,29 @@ def list_packages():
 
 	from functions.colors import colors    # REMOVE AT PACKAGING
 
-	dpkg_package_list_raw = os.popen("dpkg-query --show --showformat '${MPR-Package}/${Package}/${Version}\n'").read().splitlines()
+	dpkg_package_list_raw = os.popen("dpkg-query --show --showformat '///${MPR-Package}///${Package}///${Version}///${Description}///${Maintainer}\n'").read().splitlines()
 
 	number = 0
 	mpr_package_info = []
 
 	for i in dpkg_package_list_raw:
-		is_mpr_package = re.search('^[^/].*', i)
+		is_mpr_package = re.search('^///[^/].*', i)
 
 		if is_mpr_package != None:
 
 			number = number + 1
-			mpr_package_info += [is_mpr_package.group(0).split('/')]
+			mpr_package_info += [is_mpr_package.group(0).split('///')]
 
 	if number > 0:
 
 		number_counter = 0
 
 		for i in mpr_package_info:
-			print(f"{colors.apt_green}{i[1]}{colors.white}/{i[2]}")
-			print(f"  From: {i[0]}")
+
+			print(f"{colors.apt_green}{i[2]}{colors.white}/{i[3]}")
+			print(f"  From: {i[1]}")
+			print(f"  Description: {i[4]}")
+			print(f"  Maintainer: {i[5]}")
 
 			number_counter = number_counter + 1
 
