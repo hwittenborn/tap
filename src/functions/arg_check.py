ab8f24cf71a540dd974cd142020bd1bc54b2ed69
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-08-04 23:47:02 -0500

    Moved arguments checks into a separate function

--- src/functions/arg_check.py
+++ src/functions/arg_check.py
@@ -1,22 +1,102 @@
-def arg_check(arguments):
+def arg_check(argument_list, application_name, application_version):
 	import re
 
-	temp_arguments_list = []
+	from functions.help_menu   import  help_menu     # REMOVE AT PACKAGING
+	from functions.root_check  import  root_check    # REMOVE AT PACKAGING
+
+	if len(argument_list) == 0:
+		help_menu(application_name, application_version)
+		quit(0)
 
 	number = 0
-	number_of_arguments = len(arguments) - 1
 
-	while number <= number_of_arguments:
-		grouped_short_options = re.match('^-[a-zA-Z0-9][a-zA-Z0-9]', arguments[number])
+	argument_value = argument_list[number]
 
-		if bool(grouped_short_options) == True:
+	# Process first specified option
+	if argument_value == "-h" or argument_value == "--help":
+		help_menu(application_name, application_version)
+		quit(0)
 
-			for letter in arguments[number].replace('-', ''):
-				temp_arguments_list += ["-" + letter]
+	elif argument_value == "-V" or argument_value == "--version":
+		print(f"{application_name} ({application_version})")
+		quit(0)
 
+	elif argument_value == "install":
+		root_check()
+		operation = "install"
+
+	elif argument_value == "update" or argument_value == "upgrade":
+		root_check()
+		operation = "update"
+
+	elif argument_value == "search":
+		operation = "search"
+
+	elif argument_value == "clone":
+		operation = "clone"
+
+	elif argument_value == "list-packages":
+		operation = "list-packages"
+
+	else:
+		print(f"Unknown command '{argument_value}'")
+		quit(1)
+
+	# Specifying arrays (i.e. 'hi[1]') starts at 0 for the first argument, but
+	# len() is going to start at 1 for counting, so we need to subtract one.
+	argument_list_length = len(argument_list) - 1
+
+	# Add 1 to 'number' as we're going to start processing the *second*
+	# specified option.
+	number = number + 1
+
+	packages_temp = []
+	argument_options = []
+
+	# Loop through all remaining specified options
+	while number <= argument_list_length:
+
+		# Check for options ('-*' or '--*' strings)
+		if argument_list[number] == "-h" or argument_list[number] == "--help":
+			help_menu(application_name, application_version)
+			quit(0)
+
+		elif argument_list[number] == "-L" or argument_list[number] == "--skip-less-pipe":
+			argument_options += ["no-less-pipe"]
+
+		elif argument_list[number] == "-R" or argument_list[number] == "--rev-alpha":
+			argument_options += ["rev-alpha"]
+
+		elif argument_list[number] == "-V" or argument_list[number] == "--version":
+				print(f"{application_name} ({application_version})")
+				quit(0)
+
+		elif bool(re.match('^-.*', argument_list[number])) == True:
+			print(f"Unknown option '{argument_list[number]}'")
+			bad_argument_option = True
+
+		# Everything else is a package
 		else:
-			temp_arguments_list += [arguments[number]]
+			packages_temp += [argument_list[number]]
 
 		number = number + 1
 
-	return temp_arguments_list
+	try: bad_argument_option
+	except NameError: bad_argument_option = False
+
+	# Post-argument checks
+	if bad_argument_option == True:
+		quit(1)
+
+	elif ( operation == "update" or operation == "list-packages" ) and len(packages_temp) != 0:
+		print(f"Packages cannot be specified when using the {operation} command.")
+		quit(1)
+
+	elif (operation == "install" or operation == "search" or operation == "clone" ) and len(packages_temp) == 0:
+		print("No package was specified.")
+		quit(1)
+
+	packages = sorted(packages_temp)
+
+	# Return argument list and specified packages
+	return [operation, packages, argument_options]
--- src/main.py
+++ src/main.py
@@ -5,12 +5,11 @@ import os
 import sys
 import re
 
+from functions.split_args             import  split_args             # REMOVE AT PACKAGING
 from functions.arg_check              import  arg_check              # REMOVE AT PACKAGING
-from functions.help_menu              import  help_menu              # REMOVE AT PACKAGING
 from functions.install_package        import  install_package        # REMOVE AT PACKAGING
 from functions.update_package         import  update_package         # REMOVE AT PACKAGING
 from functions.search_package         import  search_package         # REMOVE AT PACKAGING
-from functions.root_check             import  root_check             # REMOVE AT PACKAGING
 from functions.list_packages          import  list_packages          # REMOVE AT PACKAGING
 
 # Variables we need to function
@@ -19,94 +18,12 @@ application_version = "git"
 mpr_url = "mpr.hunterwittenborn.com"
 
 # Argument check
-if len(sys.argv) == 1:
-	help_menu(application_name, application_version)
-	quit(0)
+argument_list = split_args(sys.argv[1:])
+arg_check_results = arg_check(argument_list, application_name, application_version)
 
-argument_list = arg_check(sys.argv[1:])
-
-number=0
-argument_list_length = len(argument_list) - 1
-
-argument_value = argument_list[number]
-
-if argument_value == "-h" or argument_value == "--help":
-	help_menu(application_name, application_version)
-	quit(0)
-
-elif argument_value == "-V" or argument_value == "--version":
-	print(f"{application_name} ({application_version})")
-	quit(0)
-
-elif argument_value == "install":
-	root_check()
-	operation = "install"
-
-elif argument_value == "update" or argument_value == "upgrade":
-	root_check()
-	operation = "update"
-
-elif argument_value == "search":
-	operation = "search"
-
-elif argument_value == "clone":
-	operation = "clone"
-
-elif argument_value == "list-packages":
-	operation = "list-packages"
-
-else:
-	print(f"Unknown command '{argument_value}'")
-	quit(1)
-
-number = number + 1
-
-packages_temp = []
-argument_options = []
-
-while number <= argument_list_length:
-
-	# Check for options ('-*' or '--*' strings)
-	if argument_list[number] == "-h" or argument_list[number] == "--help":
-		help_menu(application_name, application_version)
-		quit(0)
-
-	elif argument_list[number] == "-L" or argument_list[number] == "--skip-less-pipe":
-		argument_options += ["no-less-pipe"]
-
-	elif argument_list[number] == "-R" or argument_list[number] == "--rev-alpha":
-		argument_options += ["rev-alpha"]
-
-	elif argument_list[number] == "-V" or argument_list[number] == "--version":
-			print(f"{application_name} ({application_version})")
-			quit(0)
-
-	elif bool(re.match('^-.*', argument_list[number])) == True:
-		print(f"Unknown option '{argument_list[number]}'")
-		bad_argument_option = True
-
-	# Everything else is a package
-	else:
-		packages_temp += [argument_list[number]]
-
-	number = number + 1
-
-try: bad_argument_option
-except NameError: bad_argument_option = False
-
-# Post-argument checks
-if bad_argument_option == True:
-	quit(1)
-
-elif ( operation == "update" or operation == "list-packages" ) and len(packages_temp) != 0:
-	print(f"Packages cannot be specified when using the {operation} command.")
-	quit(1)
-
-elif (operation == "install" or operation == "search" or operation == "clone" ) and len(packages_temp) == 0:
-	print("No package was specified.")
-	quit(1)
-
-packages = sorted(packages_temp)
+operation = arg_check_results[0]
+packages = arg_check_results[1]
+argument_options = arg_check_results[2]
 
 # Run commands
 if operation == "install":
