d914c909fb576b594058742820eea5daf614c5ac
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-12-05 19:03:13 -0600

    Temporary workaround for installation of 'black-bin' MPR package in CI unit tests
    Looks like makedeb's '-i' toggle isn't working right now, we'll look into it more later.
    
    Also switched utilization of 'apt' to 'apt-get' in unit test script.

--- .drone/scripts/unit-tests.sh
+++ .drone/scripts/unit-tests.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 set -ex
-sudo apt install git flake8 -y
+sudo apt-get install git flake8 -y
 
 git clone "https://${mpr_url}/black-bin"
 cd black-bin/
-makedeb -si --no-confirm
+makedeb -s --no-confirm
+sudo apt-get install ./black-bin*.deb -y
+
 cd ../
 rm black-bin/ -rf
 
